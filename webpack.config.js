9cfeceb6a832589fdfc4912114a465b5c7992822
nikolay.bury <burymm@gmail.com>
2022-11-09 11:31:33 +0300

    - init commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.join(__dirname, "dist"),
+    },
+    target: 'web',
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new LiveReloadPlugin()
+    ],
+    watchOptions: {
+        ignored: /node_modules/,
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        hot: false, // optional, but you must not set both hot and liveReload to true
+        liveReload: true,
+        compress: true,
+        port: 9000,
+    },
+}
